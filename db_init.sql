a4a9cedf805d59dcaf97fb5749df5b29bd63c340
wfica <wojtekfica@gmail.com>
2018-06-02 18:21:18 +0200

    docstrings added

--- db_init.sql
+++ db_init.sql
@@ -9,7 +9,7 @@ GRANT ALL PRIVILEGES ON DATABASE student TO init;
 
 -- user app
 -- CREATE ROLE app WITH LOGIN PASSWORD 'qwerty';
--- GRANT ALL PRIVILEGES ON DATABASE student TO app;
+-- GRANT UPDATE, SELECT, DELETE, INSERT ON ALL TABLE employee TO app;
 
 -- pgcrypto for storing passwords safely 
 -- must be superuser to execute this command
--- functions_handling.py
+++ functions_handling.py
@@ -6,6 +6,8 @@ from consts_and_utils import (SECRET)
 
 
 def handle_root(args, conn):
+    """Executes function: root <secret> <newpassword> <data> <emp>"""
+
     if args['secret'] != SECRET:
         raise Exception('wrong secret')
     cur = conn.cursor()
@@ -18,6 +20,9 @@ def handle_root(args, conn):
 
 
 def check_passwd(user, passwd, conn):
+    """Checks if passwd matches users's password.
+        Returns True/False."""
+
     cur = conn.cursor()
     cur.execute(
         """SELECT id FROM employee WHERE id = %s AND passwd = crypt(%s, passwd);""", (user, passwd))
@@ -29,6 +34,8 @@ def check_passwd(user, passwd, conn):
 
 
 def handle_new(args, conn, dfsTree=None):
+    """Executes function: new <secret> <newpassword> <data> <emp>"""
+
     if check_passwd(args['admin'], args['passwd'], conn) == False:
         raise Exception('wrong password')
     if dfsTree is not None:
@@ -41,26 +48,42 @@ def handle_new(args, conn, dfsTree=None):
     cur.close()
     print('{ "status": "OK" }')
 
-def dfs(supervisor_id, conn, tree, time):
-    tree[supervisor_id] = (time, None)
+
+def dfs(supervisor_id, conn, dict, time):
+    """ Computes standard pre-order and post-order times of a dfs traverse of 
+    the employees graph. It builds dictionary - {employee:(pre-order time, post-order time)}
+    The function works in a 'functional' manner accumulating the dictionary.
+
+    Arguments:
+    supervisor_id - employee's id whose descendants graph is traversed by the dfs
+    conn - connection to the database
+    dict - 'so far' constructed dictionary
+    time - 'current' time
+
+    Returns: (dict, maxtime) where
+    dict - a dictionary which maps employee's id to pair (pre-order time, post-order time)
+    maxtime - maximum time in dict + 1"""
+
+    dict[supervisor_id] = (time, None)
     time += 1
     cur = conn.cursor()
     cur.execute(
-    """SELECT id FROM employee WHERE supervisor_id = %s""", (supervisor_id,))
+        """SELECT id FROM employee WHERE supervisor_id = %s""", (supervisor_id,))
     for id in cur:
-        tree, time = dfs(id[0], conn, tree, time )
-    (in_time, _) = tree.pop(supervisor_id)
-    tree[supervisor_id] = (in_time, time)
+        dict, time = dfs(id[0], conn, dict, time)
+    in_time, _ = dict.pop(supervisor_id)
+    dict[supervisor_id] = (in_time, time)
     time += 1
-    return tree, time 
+    return dict, time
+
 
 def preOrder_postOrder_mapping(conn):
+    """Computes standard pre-order and post-order times of a dfs traverse of the employees graph. 
+    Returns dictionary - {employee:(pre-order time, post-order time)}"""
+    
     cur = conn.cursor()
     cur.execute(
-    """SELECT id FROM employee WHERE supervisor_id IS NULL;""")
-    boss_id = cur.fetchone()
+        """SELECT id FROM employee WHERE supervisor_id IS NULL;""")
+    boss_id = cur.fetchone()[0]
     in_out_times, _ = dfs(boss_id, conn, {}, 1)
     return in_out_times
-
-
-    
